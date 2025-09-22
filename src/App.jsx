3a6c214c55e0c3b000f0291247ff4150bcedaf2d
Aywido1 <ayafolayan321@gmail.com>
2025-09-22 15:35:27 +0100

    update: newest page

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Routes>
         <Route path='/' element={Home} ></Route>
-        <Route path='/src/pages/about.jsx' element={about}></Route>
+        <Route path='/src/pages/about.jsx' element={About}></Route>
       </Routes>
     </Router>
   )
